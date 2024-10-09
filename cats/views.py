eb69fb0cf56d71feee6257003e77f05c25a73247
Danil Mordovin <Mordovin.D@yandex.ru>
2024-10-09 11:30:38 +0200

    function

--- cats/views.py
+++ cats/views.py
@@ -1,6 +1,7 @@
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import Cat
 from .serializers import CatSerializer
@@ -17,3 +18,23 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def api_cat_detail(request, pk):
+    cat = get_object_or_404(Cat, pk=pk)
+
+    if request.method == 'GET':
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    elif request.method in ['PUT', 'PATCH']:
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import cat_list, api_cat_detail
 
 urlpatterns = [
    path('cats/', cat_list),
+   path('cats/<int:pk>/', api_cat_detail),
 ]
-
-
